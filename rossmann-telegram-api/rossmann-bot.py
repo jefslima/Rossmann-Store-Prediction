49ebc4ecdc3b51f2b1879396bbd85ec3d9e48bcf
Jeferson Lima <jeferson_slima@hotmail.com>
2023-01-19 16:15:05 -0300

    16h41update

--- rossmann-telegram-api/rossmann-bot.py
+++ rossmann-telegram-api/rossmann-bot.py
@@ -42,12 +42,6 @@ def  send_message(chat_id, text, _bot, parse='HTML'):
 
     return None
 
-def send_img(chat_id, img_path, caption, _bot):
-    _bot.send_photo(chat_id, open(img_path, 'rb'), caption=caption)
-    print('Sending the file {}'.format(img_path))
-
-    return None
-
 def load_dataset(store_id=None, full=False):
     # loading test dataset
     df_test_raw = pd.read_csv('test.csv')
@@ -88,33 +82,6 @@ def predict( data ):
     d1 = pd.DataFrame( r.json(), columns=r.json()[0].keys() )
 
     return d1
-
-def draw_chart(predicted_data, x_axis, y_axis, title, x_label, y_label, img_name, divisor, tick_format='{:,.0f}'):
-
-        fig = sns.barplot(x=x_axis, y=y_axis, data=predicted_data)
-        fig.set_title(title)
-        fig.set_xlabel(x_label)
-        fig.set_ylabel(y_label)
-
-        fig.yaxis.set_major_locator(mticker.MaxNLocator(8))
-        yticks = fig.get_yticks()
-        fig.yaxis.set_major_locator(mticker.FixedLocator(yticks))
-
-
-        ylabels = [tick_format.format(x) for x in yticks / divisor]
-        fig.set_yticklabels(ylabels)
-
-        fig.figure.savefig(img_name)
-
-        print('Saving the file {}'.format(img_name))
-        print('='*20)
-        print('Params:\nx_axis: {}\ny_axis: {}\ntitle: {}\nx_label: {}\ny_label {}\nshape: {}'.format(x_axis, y_axis, title, x_label, y_label, predicted_data.shape))
-        print('='*20)
-
-        for d in predicted_data.values:
-            print(d[0])
-
-        return None
 
 def get_help(greeting=True):
     msg_help_g = ''
@@ -225,57 +192,10 @@ def index():
 
             msg_help = get_help(False)
             send_message(chat_id, msg_help, bot)
-
-        # top prediction
-        elif command == 'toppredictions':
-
-            send_message(chat_id, 'Loading. Please wait...', bot)
-
-            # loading data
-            data = load_dataset(full=True)
-
-            # prediction
-            d1 = predict(data)
-
-            d2 = d1[['store', 'prediction']].groupby('store').sum().reset_index()
-
-            d3 = d2.nlargest(5, 'prediction')
-
-            # chart definitions
-            x_ax = 'store'
-            y_ax = 'prediction'
-            chart_title = 'The Rossmann Store Highest Predictions'
-            x_lbl = 'Store ID'
-            y_lbl = 'Prediction for next 6 weeks (Unit: K)'
-            image_path = './top5_prediction.png'
-            div=1000
-            t_format='{:,.0f}K'
-
-            draw_chart(
-                d3,
-                x_axis=x_ax,
-                y_axis=y_ax,
-                title=chart_title,
-                x_label=x_lbl,
-                y_label=y_lbl,
-                img_name=image_path,
-                divisor=div,
-                tick_format=t_format
-            )
-
-            send_img(chat_id, image_path, chart_title, bot)
-
-        else:
-
-            msg_help = get_help(greeting = False)
-            send_message(chat_id, 'This is an invalid command!', bot)
-            send_message(chat_id, msg_help, bot)
-
-        send_message(chat_id, 'Done!', bot)
-        return Response('Ok', status=200)
+            return Response('Ok', status=200)
 
     else:
-           return '<h1> Rossmann Telegram BOT</h1>'
+        return '<h1> Rossmann Telegram BOT</h1>'
 
 if __name__ == '__main__':
     port = os.environ.get('PORT', 5000)
