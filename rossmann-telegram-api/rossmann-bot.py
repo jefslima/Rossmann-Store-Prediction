bfd9d4bda4dd322e24578cf061584493cbbe53cf
Jeferson Lima <jeferson_slima@hotmail.com>
2023-01-19 20:59:49 -0300

    Update rossman.py new url

--- rossmann-telegram-api/rossmann-bot.py
+++ rossmann-telegram-api/rossmann-bot.py
@@ -64,7 +64,7 @@ def load_dataset( store_id ):
 
 def predict( data ):
     # API Call
-    url = 'https://rossmann-model-test.herokuapp.com/rossmann/predict'
+    url = 'https://render-rossmann-store-api.onrender.com/rossmann/predict'
     header = {'Content-type': 'application/json' }
     data = data
 
